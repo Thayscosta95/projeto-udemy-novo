ef084d7c9082cacebf124417d8330f0eae1979ad
Thayscosta95 <tha_tacosta@hotmail.com>
2025-06-23 21:54:18 -0300

    feat: add card components with buttons and styles
    - Implemented CardComponent and CardButtonComponent with respective HTML, CSS, and spec files.
    - Created CardRoxoComponent and CardRoxoButtonComponent with their own styles and templates.
    - Updated app.component.ts to include new components as standalone.
    - Changed the language in index.html from English to Portuguese (pt-br).

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,19 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CardComponent } from "./card/card.component";
+import { CardRoxoComponent } from './card-roxo/card-roxo.component';
+import { CardButtonComponent } from './card-button/card-button.component';
+import { CardRoxoButtonComponent } from './card-roxo-button/card-roxo-button.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  standalone: true,
+  imports: [RouterOutlet,
+    CardComponent,
+    CardRoxoComponent,
+    CardButtonComponent,
+    CardRoxoButtonComponent
+    ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
